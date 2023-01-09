9522a11f2fbd3e739e5498faa869c5d9ccc1501a
joshuamichael <amjoshuamichael@gmail.com>
2023-01-09 14:09:45 -0800

    add more tests

--- tests/all.rs
+++ tests/all.rs
@@ -27,7 +27,22 @@ fn array() {
     );
 }
 
+#[derive(XcReflect)]
+struct MultidimensionaArray {
+    num: f32,
+    array: [[i32; 2]; 2],
+}
+
+#[test]
+fn multidimensional_array() {
+    assert_eq!(
+        MultidimensionaArray::alias_code(),
+        "MultidimensionaArray = { num : f32, array : [2][2]i32, }"
+    );
+}
+
 const THIRTY_TWO: usize = 32;
+const TWO: usize = 2;
 
 #[derive(XcReflect)]
 struct ArrayWithConstSize {
@@ -39,6 +54,32 @@ fn array_with_const_size() {
     assert_eq!(
         ArrayWithConstSize::alias_code(),
         "ArrayWithConstSize = { array : [32]i32, }"
+    );
+}
+
+#[derive(XcReflect)]
+struct MultiArrayWithConstSize {
+    array: [[i32; THIRTY_TWO]; TWO],
+}
+
+#[test]
+fn multi_array_with_const_size() {
+    assert_eq!(
+        MultiArrayWithConstSize::alias_code(),
+        "MultiArrayWithConstSize = { array : [2][32]i32, }"
+    );
+}
+
+#[derive(XcReflect)]
+struct MultiArrayWithExprSize {
+    array: [[i32; THIRTY_TWO * 2]; TWO + THIRTY_TWO],
+}
+
+#[test]
+fn multi_array_with_expr_size() {
+    assert_eq!(
+        MultiArrayWithExprSize::alias_code(),
+        "MultiArrayWithExprSize = { array : [34][64]i32, }"
     );
 }
 
